089e1f477c4baee943043e24162fc142243d9ad9
ekater1na <ekaterina_l@tut.by>
2023-03-14 22:29:32 +0300

    feat: add basic styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './Header.scss';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <header className="header-wrapper">
+        <nav>
+          <ul className='navbar-list'>
+            <li>
+              <a href="/">Main</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/404">404</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
