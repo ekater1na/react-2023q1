7b9f8dd9f69fdd2f6544891a932f75fd1ff2c11c
ekater1na <ekaterina_l@tut.by>
2023-03-14 22:00:45 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Routes>
+        <Route index element={<WelcomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
