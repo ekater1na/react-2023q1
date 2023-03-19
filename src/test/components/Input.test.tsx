ea5715ab936552233b2fefc2fafeb6e334f4eff6
ekater1na <ekaterina_l@tut.by>
2023-03-19 23:01:20 +0300

    feat: test input

--- src/test/components/Input.test.tsx
+++ src/test/components/Input.test.tsx
@@ -1,12 +1,28 @@
 import { describe, expect } from 'vitest';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Input from '../../components/Input/Input';
 
 describe('Input', () => {
   it('should be shown', () => {
     render(<Input />);
+    const input = document.querySelector('input') as HTMLInputElement | null;
 
     expect(screen.getByTestId('input')).toBeDefined();
+    expect(input).toBeTruthy();
+    expect(input?.textContent).toBe('');
+    if (input) {
+      input.textContent = 'jane@doe.com';
+      expect(input.textContent).toBe('jane@doe.com');
+
+      expect(input.type).toBe('text');
+
+      fireEvent.change(input, {
+        target: {
+          value: 'john@doe.com',
+        },
+      });
+      expect(input.value).toBe('john@doe.com');
+    }
   });
 });
