1e73f6bbb9c37a8f19e1890e5607a25a1715859a
ekater1na <ekaterina_l@tut.by>
2023-03-19 23:28:57 +0300

    feat: add localStorage test

--- src/test/pages/MainPage.test.tsx
+++ src/test/pages/MainPage.test.tsx
@@ -1,17 +1,29 @@
-import { describe, expect } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MainPage from '../../pages/MainPage/MainPage';
 
 describe('MainPage ', () => {
-  it('should be shown', () => {
+  beforeEach(() => {
     render(<MainPage />);
+  });
+
+  test('should be shown', () => {
     expect(screen.getByText(/characters/i)).toBeTruthy();
   });
 
-  it('should render list of cards', () => {
-    render(<MainPage />);
+  test('should render list of cards', () => {
     expect(screen.getByTestId(/main-page/i)).toBeTruthy();
     expect(screen.getAllByTestId('card')).toHaveLength(3);
+  });
+
+  test('set localStorage value', () => {
+    const setLocalStorage = (id: string, data: object) => {
+      window.localStorage.setItem(id, JSON.stringify(data));
+    };
+    const mockId = '1';
+    const mockJson = { data: 'json data' };
+    setLocalStorage(mockId, mockJson);
+    expect(localStorage.getItem(mockId)).toEqual(JSON.stringify(mockJson));
   });
 });
