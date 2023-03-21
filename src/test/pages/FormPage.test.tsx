94a9b300dd7054c3639af05c04b95672b3e0a208
ekater1na <ekaterina_l@tut.by>
2023-03-21 14:44:22 +0300

    feat: update form

---
+++ src/test/pages/FormPage.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect } from 'vitest';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FormPage from '../../pages/FormPage/FormPage';
+
+describe('FormPage', () => {
+  it('should be shown', () => {
+    render(<FormPage />);
+
+    expect(screen.getByTestId(/form-page/i)).toBeTruthy();
+  });
+});
