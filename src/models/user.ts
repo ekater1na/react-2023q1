8f9f026978f678df6b48e0d05781515a8c2c23f3
ekater1na <ekaterina_l@tut.by>
2023-03-24 23:32:04 +0300

    feat: add validation and form reset

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,11 @@
 export interface User {
-  id: number;
+  id: string;
   firstName: string | undefined;
   lastName: string | undefined;
   birthDay: string | undefined;
   country: string | undefined;
   sex: string | undefined;
   photo: string | undefined;
-  agreement: string | undefined;
+  agreement: boolean | undefined;
+  [key: string]: string | boolean | undefined;
 }
