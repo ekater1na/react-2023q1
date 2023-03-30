9d2ba867a86f66509a4c935635b559f2055a477f
ekater1na <ekaterina_l@tut.by>
2023-03-31 00:22:47 +0300

    refactor: rebase form branch

---
+++ src/test/AboutPage.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, test } from 'vitest';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AboutPage from '../pages/AboutPage/AboutPage';
+
+describe('AboutPage', () => {
+  test('should be shown', () => {
+    render(<AboutPage />);
+
+    expect(screen.getByText(/About/i)).toBeDefined();
+  });
+});
