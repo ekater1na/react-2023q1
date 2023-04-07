2f2d462afb0a419529d6266c1504858786ed6cf0
ekater1na <ekaterina_l@tut.by>
2023-04-07 18:20:51 +0300

    feat: add pagination and loader

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -13,25 +13,25 @@ export const CardItem = ({ item }: CardItemProps) => {
     <div className="col-1-of-3" data-testid="card">
       <div className="card">
         <div className="card__side card__side--front-3">
-          <div className="card__title">
-            <img className="card__image" src={image} alt={name}></img>
-          </div>
+          {/*<div className="card__title">*/}
+          {/*  <img className="card__image" src={image} alt={name}></img>*/}
+          {/*</div>*/}
           <h4 className="card__heading truncate">{name}</h4>
 
-          <div className="card__details">
-            <ul>
-              <li>{species}</li>
-              <li>{status}</li>
-              <li>{gender}</li>
-              <li>{location.name}</li>
-            </ul>
-          </div>
+          {/*<div className="card__details">*/}
+          {/*  <ul>*/}
+          {/*    <li>{species}</li>*/}
+          {/*    <li>{status}</li>*/}
+          {/*    <li>{gender}</li>*/}
+          {/*    <li>{location.name}</li>*/}
+          {/*  </ul>*/}
+          {/*</div>*/}
         </div>
         <div className="card__side card__side--back card__side--back-3">
           <div className="card__cta">
             <div className="card__price-box">
               <p className="card__price-only">Location:</p>
-              <p className="card__price-value">{location.name}</p>
+              <p className="card__price-value">{name}</p>
             </div>
             <a href="/about" className="btn btn--white">
               Visit
