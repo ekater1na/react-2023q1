835616673ea2909589d012ac9ea681cc33d03997
ekater1na <ekaterina_l@tut.by>
2023-04-08 22:20:20 +0300

    feat: add loader and pagination tests

---
+++ src/test/components/Pagination.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect } from 'vitest';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Pagination from '../../components/Pagination/Pagination';
+
+describe('Pagination', () => {
+  it('should be shown', () => {
+    render(<Pagination currentPage={1} onPageChange={() => {}} pageSize={1} totalCount={10} />);
+    expect(screen.getByTestId('pagination')).toBeDefined();
+  });
+});
