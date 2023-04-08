440f6033fde4c4ba00f93f101d7b308e7830eee2
ekater1na <ekaterina_l@tut.by>
2023-04-09 02:02:25 +0300

    feat: update options

--- src/components/OptionsBar/OptionsBar.tsx
+++ src/components/OptionsBar/OptionsBar.tsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import './OptionsBar.scss';
 
-export const OptionsBar = () => {
-  const [sortOrder, setSortOrder] = useState<string>('');
-  const [resultPerPage, setResultPerPage] = useState<number>(10);
-  const [totalCount, setTotalCount] = useState<number>(0);
+interface OptionsBarProps {
+  setSortOrder: (sortOrder: string) => void;
+  setResultPerPage: (resultPerPage: number) => void;
+  totalCount: number;
+}
+
+export const OptionsBar = ({ setSortOrder, setResultPerPage, totalCount }: OptionsBarProps) => {
+  const setResult = (e: ChangeEvent<HTMLInputElement>) => {
+    setResultPerPage(Number(e.target.value));
+  };
+  const setSort = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortOrder(e.target.value);
+  };
 
   return (
     <div className="options-bar-container" data-testid="options-bar">
       <label>
         Result per page:
-        <input type="text" name="name" />
+        <input type="text" name="name" placeholder="30 max" onChange={setResult} />
       </label>
       <label>
         Sort order:
-        <select value={sortOrder} onChange={() => setSortOrder(sortOrder)}>
-          <option value="grapefruit">Грейпфрут</option>
-          <option value="lime">Лайм</option>
-          <option value="coconut">Кокос</option>
-          <option value="mango">Манго</option>
+        <select onChange={setSort}>
+          <option value="latest">latest</option>
+          <option value="oldest">oldest</option>
+          <option value="popular">popular</option>
         </select>
       </label>
       <p>
