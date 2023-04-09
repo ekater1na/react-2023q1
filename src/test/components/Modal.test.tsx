f6f0fb588ca9489c907f274cda479e2934ff9cfa
ekater1na <ekaterina_l@tut.by>
2023-04-09 23:26:53 +0300

    fix: fix test

--- src/test/components/Modal.test.tsx
+++ src/test/components/Modal.test.tsx
@@ -7,7 +7,7 @@ import { MockResponse } from '../mockCards';
 const mockCard = MockResponse.results[0];
 describe('Modal', () => {
   it('should be shown', () => {
-    render(<Modal toggle={() => {}} visible={true} item={mockCard} />);
+    render(<Modal closeModal={() => {}} visible={true} item={mockCard} />);
     expect(screen.getByTestId('modal')).toBeDefined();
     expect(screen.getByRole('img')).toBeDefined();
     expect(screen.getByRole('button')).toBeTruthy();
