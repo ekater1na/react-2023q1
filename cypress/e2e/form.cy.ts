e55e99cf30fb8e791877919d1220714191e0268e
ekater1na <ekaterina_l@tut.by>
2023-04-29 22:25:49 +0300

    feat: update form page tests

--- cypress/e2e/form.cy.ts
+++ cypress/e2e/form.cy.ts
@@ -11,29 +11,56 @@ describe('Form page', () => {
   });
 
   it('should contain text input', () => {
-    cy.get('form')
-      .find("input[type='text']")
-      .first()
-      .should('have.attr', 'placeholder', 'First name');
+    cy.get("input[type='text']").first().should('have.attr', 'placeholder', 'First name');
 
-    cy.get('form').find("input[type='text']").first().type('ann');
+    cy.get("input[type='text']").first().type('ann');
 
-    cy.get('form').find("input[type='text']").first().should('have.value', 'ann');
+    cy.get("input[type='text']").first().should('have.value', 'ann');
   });
 
   it('should contain date input', () => {
-    cy.get('form').find("input[type='date']").should('have.attr', 'placeholder', 'Birthday');
+    cy.get("input[type='date']").should('have.attr', 'placeholder', 'Birthday');
 
-    cy.get('form').find("input[type='date']").type('2023-01-01');
+    cy.get("input[type='date']").type('2023-01-01');
 
-    cy.get('form').find("input[type='date']").should('have.value', '2023-01-01');
+    cy.get("input[type='date']").should('have.value', '2023-01-01');
   });
 
   it('should contain select', () => {
-    cy.get('form').find('select').should('have.attr', 'placeholder', 'Country');
+    cy.get('select').should('have.attr', 'placeholder', 'Country');
 
-    cy.get('form').find('select').select('Belarus');
+    cy.get('select').select('Belarus');
 
-    cy.get('form').find('select').should('have.value', 'BY');
+    cy.get('select').should('have.value', 'BY');
+  });
+
+  it('should contain file input', () => {
+    cy.get("input[type='file']").should('have.attr', 'placeholder', 'Photo');
+
+    cy.get("input[type='file']").selectFile('./src/assets/react.svg');
+  });
+
+  it('should contain errors', () => {
+    cy.get("input[type='submit']").click();
+
+    cy.get('.error-message').should('have.length', '7');
+    cy.get("input[type='submit']").should('not.be.disabled');
+  });
+
+  it('should submit form', () => {
+    cy.get("input[type='text']:first-child").type('ann');
+    cy.get("input[type='text']:nth-child(2)").type('smith');
+    cy.get("input[type='date']").type('2023-01-01');
+    cy.get('select').select('Belarus');
+    cy.get("input[type='file']").selectFile('./src/assets/react.svg');
+    cy.get("input[type='radio']").check();
+    cy.get("input[type='checkbox']").click();
+
+    cy.get('.form').submit();
+
+    cy.get('.error-message').should('have.length', '0');
+
+    cy.get('.form-card-content').should('be.visible');
+    cy.get('.form-card-content').should('have.length', '1');
   });
 });
--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -6,11 +6,45 @@ describe('Main pages', () => {
   });
 
   it('display header', () => {
-    cy.get('.header-container').should('include.text', 'Main');
+    cy.get('button').should('include.text', 'Search');
     cy.get('.header-container').should('include.text', 'Main').click();
+    cy.get('li:nth-child(4)').should('include.text', '404').click();
+    cy.get('span').should('contain', '404');
   });
-  it('display cards', () => {
+
+  it('display search-bar', () => {
+    cy.get('.header-container').should('include.text', 'Main');
+    cy.get('.search-bar-wrapper').should('be.visible');
+    cy.get('.options-bar-container').should('be.visible');
+    cy.get('.options-bar-container').should('contain', 'Total pages');
+  });
+
+  it("doesn't display cards", () => {
     cy.get('.app-container');
+    cy.get('.cards').should('have.length', 1);
     cy.get('.card').should('have.length', 0);
+  });
+
+  it(
+    'display cards',
+    {
+      defaultCommandTimeout: 10000,
+    },
+    () => {
+      cy.get('.search-bar-input').type(' ');
+      cy.get('form').submit();
+
+      cy.get('.cards').should('have.length', 1);
+      cy.get('.card').should('have.length', 20);
+    }
+  );
+
+  it('display cards with params', () => {
+    cy.get('.search-bar-input').type(' ').click();
+    cy.get('.options-bar-input').type('10').click();
+    cy.get('.options-bar-select').select('latest');
+
+    cy.get('.cards').should('have.length', 1);
+    cy.get('.card').should('have.length', 30);
   });
 });
